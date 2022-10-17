1ec3a304c1952b7937b3650aee82210ee7541a49
tonydedeveloper <tonyamazon94@gmail.com>
2022-10-17 05:52:33 -0700

    size of c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,3 +18,4 @@ printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 return (0);
+}
