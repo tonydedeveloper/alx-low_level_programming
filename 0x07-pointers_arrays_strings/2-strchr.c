cb4927f6e605d1d7aac32df17853b745cdab9e64
tonydedeveloper <tonyamazon94@gmail.com>
2022-10-31 08:58:44 -0700

    2nd

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+/*
+ * File: 2-strchr.c
+ * Auth: learner tony
+ */
+
+include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
