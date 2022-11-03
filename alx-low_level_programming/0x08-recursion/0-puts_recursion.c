fdcb388debffdc2790ef0779e124079860ec65f7
tonydedeveloper <tonyamazon94@gmail.com>
2022-11-03 03:46:54 -0700

    commit 0 and 1

--- alx-low_level_programming/0x08-recursion/0-puts_recursion.c
+++ alx-low_level_programming/0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,23 @@
 /*
- * File: 1-print_rev_recursion.c
+ * File: 0-puts_recursion.c
  * Auth: tony
  */
 
 #include "main.h"
+
 /**
- * _print_rev_recursion - Prints a string in reverse.
+ * _puts_recursion - Prints a string, followed by a new line.
  * @s: The string to be printed.
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s + 1);
 		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
+	else
+		_putchar('\n');
+
 }
