05c7947009c086c29a647ec512f95a9ef0a14c73
tonydedeveloper <tonyamazon94@gmail.com>
2022-12-08 05:39:20 -0800

    last commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,6 +29,22 @@ char *create_buffer(char *file)
 }
 
 /**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
